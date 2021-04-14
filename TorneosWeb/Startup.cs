caaa738d43479b07a9d098714279d0fe169216b0
Danoso <daniel.perezarmeria@gmail.com>
2021-04-14 01:22:13 -0500

    Dev

--- TorneosWeb/Startup.cs
+++ TorneosWeb/Startup.cs
@@ -2,9 +2,9 @@ using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using SimpleInjector;
@@ -43,6 +43,9 @@ namespace TorneosWeb
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddRazorPages();
+			services.AddControllers();
+
 			CultureInfo mex = new CultureInfo( "es-MX" );
 			CultureInfo.DefaultThreadCurrentCulture = mex;
 			CultureInfo.DefaultThreadCurrentUICulture = mex;
@@ -53,8 +56,6 @@ namespace TorneosWeb
 				 options.CheckConsentNeeded = context => true;
 				 options.MinimumSameSitePolicy = SameSiteMode.None;
 			 } );
-
-			services.AddMvc().SetCompatibilityVersion( CompatibilityVersion.Version_2_2 );
 
 			services.AddSimpleInjector( container, options =>
 			{
@@ -139,27 +140,32 @@ namespace TorneosWeb
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			app.UseSerilogRequestLogging();
-			app.UseSimpleInjector( container );
-
-			if( env.IsDevelopment() )
+			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
 			}
 			else
 			{
-				app.UseExceptionHandler( "/Error" );
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseExceptionHandler("/Error");
 				app.UseHsts();
 			}
 
+			app.UseSerilogRequestLogging();
+			app.UseSimpleInjector( container );
+
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
-			app.UseCookiePolicy();
 
-			app.UseMvc();
+			app.UseRouting();
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapRazorPages();
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Home}/{action=Index}/{id?}");
+			});
 
 			container.Verify();
 		}
