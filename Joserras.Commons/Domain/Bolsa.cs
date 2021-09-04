06a70604c177db4a9b3fe9e05f0a28189f21ba16
Danoso <daniel.perezarmeria@gmail.com>
2021-09-04 16:08:22 -0500

    New stats

--- Joserras.Commons/Domain/Bolsa.cs
+++ Joserras.Commons/Domain/Bolsa.cs
@@ -1,4 +1,6 @@
-﻿namespace Joserras.Commons.Domain
+﻿using Joserras.Commons.Exceptions;
+
+namespace Joserras.Commons.Domain
 {
 	public class Bolsa
 	{
@@ -18,6 +20,11 @@
 		public void Otorgar(decimal amount, bool fijo=false)
 		{
 			Remanente -= amount;
+			if(Remanente < 0)
+			{
+				string msg = string.Format( "Se ha otorgado más premio de lo que hay en la bolsa" );
+				throw new JoserrasException( msg );
+			}
 			if( fijo )
 			{
 				FixedForPercent -= amount;
